c1ef1fb69edc7444bc97463591ffb957b5964b78
민정 <minjaon9@gmail.com>
2022-04-15 15:13:57 +0900

    no message

---
+++ 파이썬 1.py
@@ -0,0 +1,19 @@
+score = int(input())
+
+if score >= 90:
+          print("학점 A")
+elif score >= 70 :
+          print("학점 B")
+elif score >= 40 :
+          print("학점 C")
+else >= 0 :
+          print("학점 D")
+
+
+
+
+a = int(input("숫자를 입력해주세요"))
+b = int(input("숫자를 입력해주세요"))
+for i in range(1, a+1):
+   for j in range(1, b+1):
+      print(i, j)
